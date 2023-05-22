1b22247f1d0438f16ea3a2684d516265fb11aab6
ShaidurovIlia <shaidurov.ilha@yandex.ru>
2023-05-22 10:56:18 +0200

    Initial commit

---
+++ src/main/java/net/proselyte/webFluxSecurity/WebFluxSecurityProjectApplication.java
@@ -0,0 +1,12 @@
+package net.proselyte.webFluxSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxSecurityProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/net/proselyte/webFluxSecurity/WebFluxSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proselyte.webFluxSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
